ab9a8798b29cd24892fd695ba74cdfdc28ba0623
AndoideXDD <99536529+AndoideXDD@users.noreply.github.com>
2022-11-03 21:09:32 +0100

    Update FuncioJavaScript.js

--- PaginaWeb/FuncioJavaScript.js
+++ PaginaWeb/FuncioJavaScript.js
@@ -1,10 +1,12 @@
- function PlaySound() {
+//Aquest codi es dins d'Assets a standard-page.html despres de <script>  
+
+function PlaySound() {
     //Create the audio tag
     const audio = new Audio("https://github.com/AndoideXDD/Subnormal/raw/main/correct-ding.mp3");
 	audio.play();
      
   }
-  
+  //Create the audio tag
   function MalHecho(){
     const audio = new Audio("https://github.com/AndoideXDD/Subnormal/raw/main/006150783_prev.mp3");
 	audio.play();
