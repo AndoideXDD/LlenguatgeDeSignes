824965b147c3442da6fc35a98d43287915e04130
AndoideXDD <99536529+AndoideXDD@users.noreply.github.com>
2022-11-03 21:08:13 +0100

    Create FuncioJavaScript.js

---
+++ PaginaWeb/FuncioJavaScript.js
@@ -0,0 +1,10 @@
+ function PlaySound() {
+    //Create the audio tag
+    const audio = new Audio("https://github.com/AndoideXDD/Subnormal/raw/main/correct-ding.mp3");
+	audio.play();
+     
+  }
+  
+  function MalHecho(){
+    const audio = new Audio("https://github.com/AndoideXDD/Subnormal/raw/main/006150783_prev.mp3");
+	audio.play();
